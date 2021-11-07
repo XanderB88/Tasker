ba76176d91e1c9888a2a33ab0d70360daffd989c
XanderB88 <alexxanderbykov@gmail.com>
2021-11-07 05:12:05 +0400

    Design a task screen.

---
+++ Tasker/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tasker
+//
+//  Created by Alexander on 06.11.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+   
+    @IBOutlet weak var passwordTextField: UITextField!
+  
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInButton(_ sender: UIButton) {
+    }
+   
+    @IBAction func signUpButton(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Tasker/TasksViewController.swift
@@ -0,0 +1,60 @@
+//
+//  TasksViewController.swift
+//  Tasker
+//
+//  Created by Alexander on 07.11.2021.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        navigationController?.navigationBar.barTintColor = UIColor.green
+       
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func addNewItemButton(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
+
+// MARK: - Table view data source
+extension TasksViewController: UITableViewDataSource {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        
+        cell.textLabel?.text = "Number of Row \(indexPath.row)"
+        return cell
+    }
+    
+}
+
+// MARK: - Table view delegate
+extension TasksViewController: UITableViewDelegate {
+    
+}
