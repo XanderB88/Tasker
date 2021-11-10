d530dc2cfbd0f10ecbe4479973aeb8bbf1f453c6
XanderB88 <alexxanderbykov@gmail.com>
2021-11-10 04:20:56 +0400

    Design models of application.

---
+++ Tasker/Models/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  Tasker
+//
+//  Created by Alexander on 10.11.2021.
+//
+
+import Foundation
+import Firebase
+
+struct Task {
+    var title: String
+    var userID: String
+    var ref: DatabaseReference?
+    var completed: Bool = false
+    
+    init(title: String, userID: String) {
+        self.title = title
+        self.userID = userID
+        self.ref = nil
+    }
+    
+    init(snapshot: DataSnapshot) {
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        
+        title = snapshotValue["title"] as! String
+        userID = snapshotValue["userID"] as! String
+        completed = snapshotValue["completed"] as! Bool
+        ref = snapshot.ref
+    }
+}
---
+++ Tasker/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  Tasker
+//
+//  Created by Alexander on 10.11.2021.
+//
+
+import Foundation
+import Firebase
+
+struct User {
+    var uid: String
+    var email: String
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+    }
+}
